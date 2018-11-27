82d5f97e29affc34540c8f2c400b285769f0d549
Jonathan Rasmussen <jnthnrasmussen@gmail.com>
2018-11-27 13:19:16 -0700

    Create Bee interface

--- src/apiary/Apiary.java
+++ src/apiary/Apiary.java
@@ -12,18 +12,18 @@ import beehive.Beehive;
 
 public class Apiary {
     private static Apiary single_instance = null;
-    
+
     private Apiary() {
         //TODO create hives
         System.out.println("Test");
     }
-    
+
     public static Apiary getInstance() {
         if (single_instance == null) {
             single_instance = new Apiary();
-            
+
             return single_instance;
-            
+
         } else {
             return null;
         }
---
+++ src/bee/Bee.java
@@ -0,0 +1,16 @@
+package bee;
+
+/**
+ * 
+ * @author Jonathan Rasmsusen
+ * @version 1.0
+ * @since 1.0
+ */
+
+public interface Bee {
+    
+    public void battle();
+    public void rest();
+    public void harvest();
+    
+}
